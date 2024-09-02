75024913a66e7f495531847c65e33ee5a02711d3
mohamedamara1 <hamma.amara20@gmail.com>
2024-09-02 05:15:33 +0100

    working on indiv project page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,25 +1,7 @@
-"use client";
-
-import EarthCanvas from "@/components/earth";
-import SectionWrapper from "@/hoc/SectionWrapper";
-import { slideIn, staggerContainer } from "@/utils/motion";
-import { motion } from "framer-motion";
-import { Suspense } from "react";
-
-const Contact = () => (
-  <motion.section
-    variants={staggerContainer()}
-    initial="hidden"
-    whileInView="show"
-    viewport={{ once: true, amount: 0.25 }}
-    className={`max-w-7xl mx-auto relative z-0`}
-  >
-    <span className="hash-span" id={"contact"}>
-      &nbsp;
-    </span>
-
-    <div className="flex flex-row justify-between">
-      <form className="flex flex-col gap-8 bg-gradient-to-b from-slate-700 to-gray-800  rounded-lg p-6 w-full max-w-md min-h-96 md:min-w-96">
+export default function Contact() {
+  return (
+    <div className="flex">
+      <form className="m-auto flex flex-col gap-8 bg-gradient-to-b from-slate-700 to-gray-800  rounded-lg p-6 w-full max-w-md min-h-96 md:min-w-96">
         <div className="">
           <label
             className="block text-lg font-bold mb-2 text-teal-100"
@@ -71,16 +53,6 @@ const Contact = () => (
           </button>
         </div>
       </form>
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
-      >
-        <Suspense fallback={<div className="">hi</div>}>
-          <EarthCanvas />
-        </Suspense>
-      </motion.div>
     </div>
-  </motion.section>
-);
-
-export default Contact;
+  );
+}
